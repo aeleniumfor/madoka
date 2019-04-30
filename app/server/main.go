4489bc54dfb223885a7b03077dffd6ea7ff45c80
aeleniumfor <kuzu.kamo@gmail.com>
2019-04-30 21:49:06 +0900

    Add echo

--- app/server/main.go
+++ app/server/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo"
 	"fmt"
 )
 
 func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+
 	fmt.Println("madoka")
+	e.Start(":1323")
 }

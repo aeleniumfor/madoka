461af6b185eea1f681075b3abbd7994827d28f5e
aeleniumfor <kuzu.kamo@gmail.com>
2019-05-02 03:11:48 +0900

    Comment out

--- app/server/main.go
+++ app/server/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"github.com/labstack/echo/middleware"
-	"github.com/labstack/echo"
-	"github.com/madoka/app/handler"
-	"fmt"
+	// "github.com/labstack/echo/middleware"
+	// "github.com/labstack/echo"
+	// "github.com/madoka/app/handler"
+	// "fmt"
 )
 
 func main() {
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	// e := echo.New()
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
 
-	e.GET("/auth/test",handler.AuthTest())
-	e.POST("/container/network",handler.AddNetworkHandler())
+	// e.GET("/auth/test",handler.AuthTest())
+	// e.POST("/container/network",handler.AddNetworkHandler())
 
-	fmt.Println("madoka")
-	e.Start(":8081")
+	// fmt.Println("madoka")
+	// e.Start(":8081")
+	
 }

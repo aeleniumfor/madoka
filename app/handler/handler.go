c8b78f76639c409a40a0f3eed0c645dbe5b0b0b0
aeleniumfor <kuzu.kamo@gmail.com>
2019-05-19 01:04:42 +0900

    Change

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -1,43 +1,15 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/labstack/echo"
-	"net/http"
+	
 )
 
-// Net is controll network
-type Net struct {
-	ContainerID string `json:"container_id"`
-}
+
 
 // AuthTest is AurtTest
 func AuthTest() echo.HandlerFunc {
 	return func(c echo.Context) error { // なにか考える
-		return c.String(http.StatusOK, "")
-	}
-}
-
-// AddNetworkHandler is network Adding
-func AddNetworkHandler() echo.HandlerFunc {
-	return func(c echo.Context) error { // コンテナへIPを追加
-		u := new(Net)
-		if err := c.Bind(u); err != nil {
-			return err
-        }
-        fmt.Println(u.ContainerID)
-		return c.JSON(http.StatusOK, u)
-	}
-}
-
-// DelNetworkHandler is network Adding
-func DelNetworkHandler() echo.HandlerFunc {
-	return func(c echo.Context) error { // コンテナへIPを追加
-		u := new(Net)
-		if err := c.Bind(u); err != nil {
-			return err
-        }
-        fmt.Println(u.ContainerID)
-		return c.JSON(http.StatusOK, u)
+		return c.String(200, "")
 	}
 }

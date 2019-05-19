a2b35961fce4d8bc4106a655ae609e8c94f95fdb
aeleniumfor <kuzu.kamo@gmail.com>
2019-05-19 21:55:18 +0900

    Add handler

--- app/requests/requests.go
+++ app/requests/requests.go
@@ -3,12 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/madoka/app/common"
 	"io/ioutil"
 	"net/http"
-	"github.com/madoka/app/common"
 )
 
-// CreateContainerRequests is Requests 
+// CreateContainerRequests is Requests
 func CreateContainerRequests() {
 	client := &http.Client{}
 	url := "http://localhost:8080/container/list/id"
@@ -22,14 +22,12 @@ func CreateContainerRequests() {
 	fmt.Println(err)
 	data, _ := ioutil.ReadAll(res.Body)
 
-	containerIDList:= new(common.ResCotainerIDList)
+	containerIDList := new(common.ResCotainerIDList)
 	json.Unmarshal(data, &containerIDList)
 	res.Body.Close()
 	fmt.Println(*containerIDList)
 }
 
-
-
-func main(){
+func main() {
 	CreateContainerRequests()
 }

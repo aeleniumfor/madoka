5b2b5d931c3a173d3a569e161406ba1e86083189
aeleniumfor <kuzu.kamo@gmail.com>
2019-05-19 19:33:29 +0900

    Copy templete common from viqutrique

--- app/common/common.go
+++ app/common/common.go
@@ -1,2 +1,29 @@
 package common
 
+// Container is
+type Container struct {
+	Name         string `json:"Container_Name"`
+	ID           string `json:"Container_ID"`
+	NetNamespace string `json:"Container_NetNamespace"`
+	Status       string `json:"Container_Status"`
+}
+
+// ResCotainerIDList is
+type ResCotainerIDList struct {
+	ContainerIDList []string `json:"Container_ID_List"`
+}
+
+// ResCotainerNameList is
+type ResCotainerNameList struct {
+	ContainerNameList []string `json:"Container_Name_List"`
+}
+
+// ReqContainerID is
+type ReqContainerID struct {
+	ContainerID []string `json:"Container_ID"`
+}
+
+// ResContainerDelMsg is delete container message
+type ResContainerDelMsg struct {
+	Msg string `json:"Container_Delete_Msg"`
+}
